551a302738336fb513af04c4c1804bc9e1581f56
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-06-23 18:01:33 -0400

    add simple pages for games, movies, and shows with basic headers and lists

---
+++ frontend/app/movies/page.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+    return (
+        <div className="mx-auto max-w-7xl py-10 px-6 lg:px-8 flex flex-col items-center">
+            <div className="text-3xl mb-8 mt:4 sm:mt-6">Movie Recommendations</div>
+            <ul role="list" className="space-y-3 w-[90vw] sm:w-[600px] max-w-5xl flex flex-col">
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                    Something
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                    Something
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                    Something
+                </li>
+            </ul>
+        </div>
+    )
+  }
