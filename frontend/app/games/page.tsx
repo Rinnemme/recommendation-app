39cf8b1ccbed3fe0d77bc9e2df8472d53da0dc7b
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-06-24 15:49:02 -0400

    add a fade-in class with keyframes for each page, make the headers extra-light, and each rec title regular weight

--- frontend/app/games/page.tsx
+++ frontend/app/games/page.tsx
@@ -7,14 +7,14 @@ export default function Page() {
     const {games} = useContext(recContext)
 
     return (
-        <div className="mx-auto max-w-7xl py-10 px-6 lg:px-8 flex flex-col items-center">
-            <div className="text-3xl mb-10 mt:4 sm:mt-6 sm:mb-12 text-slate-700 font-light">Game Recommendations</div>
+        <div className="mx-auto max-w-7xl py-10 px-6 lg:px-8 flex flex-col items-center fade-in">
+            <div className="text-3xl mb-10 mt:4 sm:mt-6 sm:mb-12 text-slate-700 font-extralight">Game Recommendations</div>
             <ul role="list" className="space-y-3 w-[90vw] sm:w-[600px] max-w-5xl flex flex-col gap-3 mb-12">
                 {games.map((game) => {
                     const url = game.steamLink
                     return (
                         <li className="overflow-hidden bg-white px-6 py-6 shadow sm:rounded-md sm:px-6 relative">
-                            <p className="text-xl font-semibold text-teal-600">{game.name}</p>
+                            <p className="text-xl text-teal-600">{game.name}</p>
                             <p className="absolute right-6 top-6 text-slate-500">{`${game.releaseYear}`}</p>
                             <p className="text-sm mt-4"><span className="font-bold">{`Genre(s): `}</span>{`${game.genre.join(', ')}`}</p>
                             <p className="text-sm mt-1"><span className="font-bold">{`Combat: `}</span>{`${game.combat}`}</p>
--- frontend/app/shows/page.tsx
+++ frontend/app/shows/page.tsx
@@ -7,13 +7,13 @@ export default function Page() {
     const {shows} = useContext(recContext)
 
     return (
-        <div className="mx-auto max-w-7xl py-10 px-6 lg:px-8 flex flex-col items-center">
-            <div className="text-3xl mb-10 mt:4 sm:mt-6 sm:mb-12 text-slate-700 font-light">Show Recommendations</div>
+        <div className="mx-auto max-w-7xl py-10 px-6 lg:px-8 flex flex-col items-center fade-in">
+            <div className="text-3xl mb-10 mt:4 sm:mt-6 sm:mb-12 text-slate-700 font-extralight">Show Recommendations</div>
             <ul role="list" className="space-y-3 w-[90vw] sm:w-[600px] max-w-5xl flex flex-col gap-3 mb-12">
                 {shows.map((show) => {
                     return (
                         <li className="overflow-hidden bg-white px-6 py-6 shadow sm:rounded-md sm:px-6 relative">
-                            <p className="text-xl font-semibold text-teal-600">{show.name}</p>
+                            <p className="text-xl text-teal-600">{show.name}</p>
                             <p className="absolute right-6 top-6 text-slate-500">{`${show.releaseYear}`} {show.endYear ? ` - ${show.endYear}`: ` - present`}</p>
                             <p className="text-sm mt-4"><span className="font-bold">{`Watch on: `}</span>{`${show.platform.join(', ')}`}</p>
                             <p className="text-sm mt-1"><span className="font-bold">{`Genre(s): `}</span>{`${show.genre.join(', ')}`}</p>
