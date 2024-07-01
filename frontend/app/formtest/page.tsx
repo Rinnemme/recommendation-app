7e6c79309b8aea8a70e982ce5562da14c63babb3
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-01 15:58:47 -0400

    add basic validation to input fields

--- frontend/app/formtest/page.tsx
+++ frontend/app/formtest/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
-import { useState } from 'react'
+import { useState, ReactNode } from 'react'
 import Selector from '@/components/Selector'
+import { movieRec } from '@/types'
+import { useForm } from "react-hook-form"
 
 export default function Page() {
     const [genreList, setGenreList] = useState<string[] | []>([])
     const [platformList, setPlatformList] = useState<string[] | []>([])
+
+    const {register, handleSubmit, formState} = useForm<movieRec>({mode: "onTouched"})
+    const {errors} = formState
 
     const toggleGenre = (genre:string) => {
         if(genreList.includes(genre as never)) {
@@ -80,30 +85,44 @@ export default function Page() {
                         <div className="mt-2">
                             <input
                             type="text"
-                            name="name"
                             id="name"
                             className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
                             placeholder="e.g. Dead Poet's Society"
+                            {...register("name", {
+                                required: "Movie name is required",
+                                pattern: {
+                                    value: /^[^<>{}!>]*$/,
+                                    message: "Must not contain: <, {, >, }"
+                                }
+                            })}
                             />
                         </div>
+                        <p className="mt-1 text-sm h-2 text-red-600">{errors.name?.message as ReactNode}</p>
                     </div>
 
                     <div className="w-72 mt-6">
-                        <label htmlFor="name" className="block font-medium leading-6 text-gray-900">
+                        <label htmlFor="stars" className="block font-medium leading-6 text-gray-900">
                             Stars
                         </label>
                         <div className="mt-2">
                             <input
                             type="text"
-                            name="name"
-                            id="name"
+                            id="stars"
                             className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
                             placeholder="e.g. Tom Cruise, Tom Hanks, Tom Hardy"
+                            {...register("starring", {
+                                required: "At least one star is required",
+                                pattern: {
+                                    value: /^[^<>{}!>]*$/,
+                                    message: "Must not contain: <, {, >, }"
+                                }
+                            })}
                             />
                         </div>
                         <p className="mt-2 text-sm text-gray-500" id="email-description">
                             Separate names with a comma and space
                         </p>
+                        <p className="mt-1 text-sm h-2 text-red-600">{errors.starring?.message as ReactNode}</p>
                     </div>
 
                     <div className="w-72 mt-6">
@@ -113,12 +132,19 @@ export default function Page() {
                         <div className="mt-2">
                             <input
                             type="text"
-                            name="director"
                             id="director"
                             className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
                             placeholder="e.g. Ridley Scott"
+                            {...register("director", {
+                                required: "Director is required",
+                                pattern: {
+                                    value: /^[^<>{}!>]*$/,
+                                    message: "Must not contain: <, {, >, }"
+                                }
+                            })}
                             />
                         </div>
+                        <p className="mt-1 text-sm h-2 text-red-600">{errors.director?.message as ReactNode}</p>
                     </div>
 
                     <div className="w-72 mt-6">
@@ -128,12 +154,19 @@ export default function Page() {
                         <div className="mt-2">
                             <input
                             type="text"
-                            name="length"
                             id="length"
                             className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
                             placeholder="e.g. 129 for a movie that is 2hr 9m"
+                            {...register("length", {
+                                required: "Length is required",
+                                pattern: {
+                                    value: /^[0-9]*$/,
+                                    message: "Must be only a number"
+                                }
+                            })}
                             />
                         </div>
+                        <p className="mt-1 text-sm h-2 text-red-600">{errors.length?.message as ReactNode}</p>
                     </div>
 
 
@@ -147,13 +180,20 @@ export default function Page() {
                         <div className="mt-2">
                             <textarea
                             id="about"
-                            name="about"
                             rows={3}
                             className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
                             defaultValue={''}
+                            {...register("synopsis", {
+                                required: "Synopsis is required",
+                                pattern: {
+                                    value: /^[^<>{}!>]*$/,
+                                    message: "Must not contain: <, {, >, }"
+                                }
+                            })}
                             />
                         </div>
                         <p className="mt-3 text-sm leading-6 text-gray-600">What's it about? What themes does it address? What does it make you feel? Give your pitch!</p>
+                        <p className="mt-1 text-sm h-2 text-red-600">{errors.synopsis?.message as ReactNode}</p>
                     </div>
 
                 </div>
