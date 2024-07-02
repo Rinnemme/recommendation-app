b1246703ab638adf0221a88730f3c3f3fe76be30
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-02 14:53:13 -0400

    add lists of genres and platforms to a separate ts for imports for submission forms

---
+++ frontend/lists.ts
@@ -0,0 +1,31 @@
+const videoGenres:{id:number, name:string}[] = [
+    {id: 1, name: 'Horror'},
+    {id: 2, name: 'Action'},
+    {id: 3, name: 'Drama'},
+    {id: 4, name: 'Romance'},
+    {id: 5, name: 'Comedy'},
+    {id: 6, name: 'Scifi'},
+    {id: 7, name: 'Western'},
+    {id: 8, name: 'Fantasy'},
+    {id: 9, name: 'Crime'},
+    {id: 10, name: 'Psychological Thriller'},
+    {id: 11, name: 'Adventure'},
+    {id: 12, name: 'Documentary'},
+    {id: 13, name: 'Musical'},
+    {id: 14, name: 'Fighting'}
+]
+
+const streamingPlatforms:{id:number, name:string}[] = [
+    {id: 1, name: 'Hulu'},
+    {id: 2, name: 'Netflix'},
+    {id: 3, name: 'Apple TV'},
+    {id: 4, name: 'Disney +'},
+    {id: 5, name: 'Amazon Prime Video'},
+    {id: 6, name: 'Max'},
+    {id: 7, name: 'Paramount'},
+    {id: 8, name: 'Peacock'},
+    {id: 9, name: 'Crunchyroll'},
+    {id: 10, name: 'Hi Dive'}
+]
+
+export {videoGenres, streamingPlatforms}
