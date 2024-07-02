f72f877b2bbfccd6309a3a08c0bdda5c574e278e
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-02 17:16:04 -0400

    remove extraneous wrapping divs from mapped elements in Selector

--- frontend/components/Selector.tsx
+++ frontend/components/Selector.tsx
@@ -9,10 +9,8 @@ export default function Selector ({array, state, func}: Readonly<{array: {id:num
                     className={"relative flex w-[180px] h-[40px] text-center px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer " + (state.includes(item.name as never) ? "bg-teal-300" : "bg-slate-100")}
                     onClick={() => func(item.name)}
                 >
-                    <div className="min-w-0 flex-1 text-sm leading-6 hover:cursor-pointer">
-                    <label htmlFor={`item-${item.id}`} className="select-none font-medium text-gray-900 hover:cursor-pointer">
+                    <div className="min-w-0 flex-1 text-sm font-medium text-slate-900 leading-6 hover:cursor-pointer">
                         {item.name}
-                    </label>
                     </div>
                 </div>
                 ))}
