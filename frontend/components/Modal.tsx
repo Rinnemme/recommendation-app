3623047dda55bb6f13096f9bba8771a49516c4bd
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-05 14:12:33 -0400

    fix names of html properties for svg on Modal, e.g. stroke-linecap to strokeLinecap

--- frontend/components/Modal.tsx
+++ frontend/components/Modal.tsx
@@ -16,8 +16,8 @@ export default function Modal({children, closeFunc}: Readonly<{children: React.R
             className="flex flex-col relative content-center rounded-md bg-white px-4 pb-4 pt-5 max-w-xl max-h-[90dvh] overflow-scroll my-12"
           >
             <div className="sticky top-0">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.75" className="absolute top-0 right-0 size-6 stroke-slate-400 hover:cursor-pointer hover:stroke-rose-500" onClick={closeFunc}>
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="0.75" className="absolute top-0 right-0 size-6 stroke-slate-400 hover:cursor-pointer hover:stroke-rose-500" onClick={closeFunc}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
               </svg>
             </div>
             {children}
