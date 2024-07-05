cbf108cba4bfd3dc204b6bcc59dac39b2fe3365b
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-05 11:12:38 -0400

    add gameGenres list to lists.ts

--- frontend/lists.ts
+++ frontend/lists.ts
@@ -28,4 +28,21 @@ const streamingPlatforms:{id:number, name:string}[] = [
     {id: 10, name: 'Hi Dive'}
 ]
 
-export {videoGenres, streamingPlatforms}
+const gameGenres:{id:number, name:string}[] = [
+    {id: 1, name: 'RPG'},
+    {id: 2, name: 'Action'},
+    {id: 3, name: 'Adventure'},
+    {id: 4, name: 'Simulation'},
+    {id: 5, name: 'Fighting'},
+    {id: 6, name: 'Sports'},
+    {id: 7, name: 'Strategy'},
+    {id: 8, name: 'Fantasy'},
+    {id: 9, name: 'FPS'},
+    {id: 10, name: 'Platformer'},
+    {id: 11, name: 'Survival'},
+    {id: 12, name: 'Horror'},
+    {id: 13, name: 'Racing'},
+    {id: 14, name: 'Sidescroller'}
+]
+
+export {videoGenres, streamingPlatforms, gameGenres}
