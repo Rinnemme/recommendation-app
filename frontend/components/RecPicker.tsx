4e64094f3bbb4343519f1da1d9822f012ecae5db
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-06 15:46:33 -0400

    adjust RecPicker header to slate-700

--- frontend/components/RecPicker.tsx
+++ frontend/components/RecPicker.tsx
@@ -3,7 +3,7 @@ import { Dispatch, SetStateAction } from "react";
 export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetStateAction<any>>}>) {
     return (
         <fieldset className="p-4">
-            <div className="text-3xl font-extralight leading-normal mb-8 text-teal-600">Recommend a:</div>
+            <div className="text-3xl font-extralight leading-normal mb-8 text-slate-700">Recommend a:</div>
             <div className="mt-1 flex flex-wrap items-center justify-center gap-4">
                 <div 
                     className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-600 hover:bg-sky-600"}
