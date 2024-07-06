75b57f5881e4642d41e94bc825d884d531936efd
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-06 15:34:59 -0400

    fix onClicks of buttons back to correctly setting recommending state

--- frontend/components/RecPicker.tsx
+++ frontend/components/RecPicker.tsx
@@ -3,7 +3,7 @@ import { Dispatch, SetStateAction } from "react";
 export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetStateAction<any>>}>) {
     return (
         <fieldset className="p-4">
-            <div className="text-3xl font-extralight underline underline-offset-8 decoration-teal-500 decoration-1 leading-normal mb-8 text-sky-800">Recommend a:</div>
+            <div className="text-3xl font-extralight leading-normal mb-8 text-sky-800">Recommend a:</div>
             <div className="mt-1 flex flex-wrap items-center justify-center gap-4">
                 <div 
                 // relative inline-flex items-center gap-x-1.5 rounded-md bg-teal-500 px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 hover:scale-105 transition-all ease-in-out duration-300
@@ -17,7 +17,7 @@ export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetState
                 <div 
                     className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-sky-600 hover:bg-teal-500"}
                     >
-                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={() => setFunc('Show')}>
+                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={() => setFunc('Movie')}>
                         Movie
                     </div>
                 </div>
@@ -25,7 +25,7 @@ export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetState
                 <div 
                     className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-sky-600 hover:bg-teal-500"}
                     >
-                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={() => setFunc('Show')}>
+                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={() => setFunc('Game')}>
                         Game
                     </div>
                 </div>
