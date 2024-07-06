9eabb7ceae5e5e0851dfe314af0573e14fba851d
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-06 15:32:59 -0400

    make RecPicker and Success buttons styled similarly to rest of app

--- frontend/components/RecPicker.tsx
+++ frontend/components/RecPicker.tsx
@@ -6,25 +6,26 @@ export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetState
             <div className="text-3xl font-extralight underline underline-offset-8 decoration-teal-500 decoration-1 leading-normal mb-8 text-sky-800">Recommend a:</div>
             <div className="mt-1 flex flex-wrap items-center justify-center gap-4">
                 <div 
-                    className={"relative flex w-[90px] h-[40px] text-center px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-300"}
+                // relative inline-flex items-center gap-x-1.5 rounded-md bg-teal-500 px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 hover:scale-105 transition-all ease-in-out duration-300
+                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-sky-600 hover:bg-teal-500"}
                 >
-                    <div className="min-w-0 flex-1 text-sm font-medium text-slate-900 leading-6 hover:cursor-pointer" onClick={() => setFunc('Show')}>
+                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={() => setFunc('Show')}>
                         Show
                     </div>
                 </div>
 
                 <div 
-                    className={"relative flex w-[90px] h-[40px] text-center px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-300"}
-                >
-                    <div className="min-w-0 flex-1 text-sm font-medium text-slate-900 leading-6 hover:cursor-pointer" onClick={() => setFunc('Movie')}>
+                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-sky-600 hover:bg-teal-500"}
+                    >
+                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={() => setFunc('Show')}>
                         Movie
                     </div>
                 </div>
 
                 <div 
-                    className={"relative flex w-[90px] h-[40px] text-center px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-300"}
-                >
-                    <div className="min-w-0 flex-1 text-sm font-medium text-slate-900 leading-6 hover:cursor-pointer" onClick={() => setFunc('Game')}>
+                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-sky-600 hover:bg-teal-500"}
+                    >
+                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={() => setFunc('Show')}>
                         Game
                     </div>
                 </div>
--- frontend/components/Success.tsx
+++ frontend/components/Success.tsx
@@ -5,9 +5,9 @@ export default function RecPicker ({closeFunc}: Readonly<{closeFunc: () => void 
             <div className="text-l font-light underline-offset-8 decoration-teal-500 leading-normal mb-6 text-slate-900">Thanks for your recommendation!</div>
             <div className="mt-1 flex flex-wrap items-center justify-center gap-4">
                 <div 
-                    className={"relative flex w-[90px] h-[40px] text-center px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-300"}
-                >
-                    <div className="min-w-0 flex-1 text-sm font-medium text-slate-900 leading-6 hover:cursor-pointer" onClick={closeFunc}>
+                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-sky-600 hover:bg-teal-500"}
+                    >
+                    <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer" onClick={closeFunc}>
                         OK
                     </div>
                 </div>
