d3bebe0ec2e56f15c3b96db0120110e12ccab25c
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-06 16:07:59 -0400

    Adjust button default color from teal-600 to teal-500 to brighten RecPicker

--- frontend/components/RecPicker.tsx
+++ frontend/components/RecPicker.tsx
@@ -6,7 +6,7 @@ export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetState
             <div className="text-3xl font-extralight leading-normal mb-8 text-slate-700">Recommend a:</div>
             <div className="mt-1 flex flex-wrap items-center justify-center gap-4">
                 <div 
-                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-600 hover:bg-sky-600"} onClick={() => setFunc('Show')}
+                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-500 hover:bg-sky-600"} onClick={() => setFunc('Show')}
                 >
                     <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer">
                         Show
@@ -14,7 +14,7 @@ export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetState
                 </div>
 
                 <div 
-                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-600 hover:bg-sky-600"} onClick={() => setFunc('Movie')}
+                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-500 hover:bg-sky-600"} onClick={() => setFunc('Movie')}
                     >
                     <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer">
                         Movie
@@ -22,7 +22,7 @@ export default function RecPicker ({setFunc}:Readonly<{setFunc:Dispatch<SetState
                 </div>
 
                 <div 
-                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-600 hover:bg-sky-600"} onClick={() => setFunc('Game')}
+                    className={"relative flex w-[90px] h-[40px] text-center rounded-md px-4 items-center py-4 hover:shadow-md transition-all duration-300 hover:scale-105 hover:cursor-pointer bg-teal-500 hover:bg-sky-600"} onClick={() => setFunc('Game')}
                     >
                     <div className="min-w-0 flex-1 text-sm font-semibold text-white leading-6 hover:cursor-pointer">
                         Game
