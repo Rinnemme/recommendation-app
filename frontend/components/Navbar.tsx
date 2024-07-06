d7f07b954ebf7ef5cfa518ddd726f8d637d5b089
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-07-06 16:05:52 -0400

    change universal hover cursor to default and button hover to pointer, adjusting svgs within navbar and modal to have pointer on hover as well

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -51,33 +51,33 @@ export default function Navbar() {
                   {/* Mobile menu button */}
                   <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="absolute -inset-0.5" />
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 hover:cursor-pointer">
+                        <path className="hover:cursor-pointer" strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                   </DisclosureButton>
                 </div>
 
-                <div className="flex flex-shrink-0 items-center">
+                <div className="flex flex-shrink-0 items-center hover:cursor-pointer">
                     <Link href="/">
-                        <Image className="h-10 w-auto" src={Logo} alt="Nerdy Recs"></Image>
+                        <Image className="h-10 w-auto hover:cursor-pointer" src={Logo} alt="Nerdy Recs"></Image>
                     </Link>
                 </div>
 
                 <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-                    <Link href="/games" className={"rounded-md px-3 py-2 text-sm " + (path === "/games" ? "bg-gradient-to-r from-teal-500 to-sky-700 text-white" : 'text-gray-800 hover:text-cyan-700 hover:bg-slate-100')}>Games</Link>
-                    <Link href="/movies" className={"rounded-md px-3 py-2 text-sm " + (path === "/movies" ? "bg-gradient-to-r from-teal-500 to-sky-700 text-white" : 'text-gray-800 hover:text-cyan-700 hover:bg-slate-100')}>Movies</Link>
-                    <Link href="/shows" className={"rounded-md px-3 py-2 text-sm " + (path === "/shows" ? "bg-gradient-to-r from-teal-500 to-sky-700 text-white" : 'text-gray-800 hhover:text-cyan-700 hover:text-cyan-700 hover:bg-slate-100')}>Shows</Link>
+                    <Link href="/games" className={"rounded-md px-3 py-2 text-sm hover:cursor-pointer " + (path === "/games" ? "bg-gradient-to-r from-teal-500 to-sky-700 text-white" : 'text-gray-800 hover:text-cyan-700 hover:bg-slate-100')}>Games</Link>
+                    <Link href="/movies" className={"rounded-md px-3 py-2 text-sm hover:cursor-pointer " + (path === "/movies" ? "bg-gradient-to-r from-teal-500 to-sky-700 text-white" : 'text-gray-800 hover:text-cyan-700 hover:bg-slate-100')}>Movies</Link>
+                    <Link href="/shows" className={"rounded-md px-3 py-2 text-sm hover:cursor-pointer " + (path === "/shows" ? "bg-gradient-to-r from-teal-500 to-sky-700 text-white" : 'text-gray-800 hhover:text-cyan-700 hover:text-cyan-700 hover:bg-slate-100')}>Shows</Link>
                 </div>
               </div>
 
-              <div className="flex items-center">
+              <div className="flex items-center hover:cursor-pointer">
                 <div className="flex-shrink-0">
                   <button
                     type="button"
-                    className="relative inline-flex items-center gap-x-1.5 rounded-md bg-teal-500 px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 hover:scale-105 transition-all ease-in-out duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    className="relative inline-flex items-center gap-x-1.5 rounded-md bg-teal-500 px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 hover:scale-105 hover:cursor-pointer transition-all ease-in-out duration-300"
                     onClick={() => setModalOpen(true)}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 hover:cursor-pointer">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     Recommend
